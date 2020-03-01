71c261669572444b084eb7a674a0d6ee91596461
Hal Shin <hal.shin@alumni.ubc.ca>
2020-02-29 16:24:31 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import Boards from "./components/boards";
+import DraggableCollection from "./components/DraggableCollection";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      boards: 2
-    };
-  }
-
-  handleNewTable = () => {
-    if (this.state.boards < 4) {
-      this.setState({
-        boards: this.state.boards + 1
-      });
-    }
-  };
-
   render() {
-    var boards = [];
-    for (let i = 0; i < this.state.boards; i++) {
-      boards.push(<Boards />);
-    }
     return (
       <div className="App">
-        <h1>Trello</h1>
-        <div id="container">
-          {boards}
-          <div onClick={this.handleNewTable}>+ Add a New Board</div>
-        </div>
+        <DraggableCollection />
       </div>
     );
   }
---
+++ src/components/DraggableCollection.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+
+class DraggableCollection extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default DraggableCollection;
